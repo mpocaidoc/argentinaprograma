5a969fd458949e9f27258d9cedcf3c287366c336
Mati Pocai <matiaspocaidocencia@gmail.com>
2023-05-23 23:24:09 -0300

    commit desde consola linuk ssh :)

---
+++ practico0/multiplicacion.js
@@ -0,0 +1,5 @@
+let num1=2;
+let num2=17;
+let num3=6;
+
+console.log(num1*num2*num3);//Resultado 204
---
+++ practico0/resta.js
@@ -0,0 +1,5 @@
+
+let minuendo = 10;
+let sustraendo = 2;
+let resultado = minuendo - sustraendo;
+console.log('Resultado de resta 10 - 2: '+resultado); //devuelve 8
