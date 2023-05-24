c3245a2ab7476782432c9bcd1fa0cda8f16623e9
Mati Pocai <matiaspocaidocencia@gmail.com>
2023-05-23 23:26:52 -0300

    practico no finalizado

---
+++ practico0/areaRectangulo.js
@@ -0,0 +1,6 @@
+let base=12.5;
+let altura=5.3;
+let area = base*altura;
+
+console.log('El área del rectángulo es: '+area);
+//da 15 en el primer caso, y 66.25 el último, el atual
---
+++ practico0/promedio3.js
@@ -0,0 +1,6 @@
+let x=-1.2;
+let z=5;
+let y=-3.3;
+let promedio = (x+y+z)/3;
+console.log(promedio);
+//rta: 7, -4,33 y -0,1666 respectivamente. 
